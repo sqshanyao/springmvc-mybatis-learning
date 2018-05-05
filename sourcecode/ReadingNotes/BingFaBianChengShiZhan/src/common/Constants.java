b361780ce63765c936747c8a7643cf20a6583e28
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-05-05 23:33:17 +0800

    1

---
+++ sourcecode/ReadingNotes/BingFaBianChengShiZhan/src/common/Constants.java
@@ -0,0 +1,9 @@
+package common;
+
+/**
+ * Created by shanyao on 2018/4/19.
+ */
+public class Constants {
+    public static long SLEEP_GRANULARITY = 1000;
+    public static int BUFFER_SIZE =10;
+}
